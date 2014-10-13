b2f96724c058d5873b7b171e7b47bb194fb59a22
whitehawk <white-hawk-taka@outlook.com>
2014-10-13 18:01:43 +0900

    コメント削除及び空行削除

--- Cal.java
+++ Cal.java
@@ -1,26 +1,15 @@
 import java.util.Calendar;
-
 public class Cal{
     public static void main(String[] args){
-        int count,week;
-        int i,j;
-        
+        int count,week,i,j;
         Calendar cal = Calendar.getInstance();
-        
-        //曜日を取得
         cal.set(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),1);
         week = cal.get(Calendar.DAY_OF_WEEK);
-        
-        //最終日を取得
         cal.set(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH) + 1,0);
         count = cal.get(Calendar.DATE);
-        
-        //月初めの空白を挿入
         for(i = 1; i < week; i++){
             System.out.print("   ");
         }
-        
-        
         for(j = 0; j < count; j++){
             System.out.print(String.format("%1$2d ", j+1));
             i++;
